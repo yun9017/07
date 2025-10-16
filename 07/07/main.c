8e6c587eb23f3193af451ba8351b7b6a825694cc
yun9017 <yun9017ie@naver.com>
2025-10-16 16:09:41 +0900

    07

--- 07/07/main.c
+++ 07/07/main.c
@@ -6,24 +6,14 @@
 //
 
 #include <stdio.h>
+int all_files;
 
-void sub(void);
+extern void sub();
 
 int main(void)
 {
-    int i;
-    for (i=0; i<3; i++)
-        sub();
+    sub(); // extern 통해 다른 파일 함수 연결
+    printf("%d\n",all_files);
     return 0;
     }
-
-void sub(void)
-{
-    int auto_count = 0;
-    static int static_count = 0;
-    auto_count++;
-    static_count++;
-    printf("auto_count = %d\n",auto_count);
-    printf("static_count = %d\n",static_count);
-}
 
---
+++ 07/07/sub.c
@@ -0,0 +1,12 @@
+//
+//  sub.c
+//  07
+//
+//  Created by 한서윤 on 10/16/25.
+//
+
+extern int all_files;
+void sub(void)
+{
+    all_files = 10;
+}
