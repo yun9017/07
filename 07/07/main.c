364717c49b4c65795f95d8169d6e1c5d0cf22c1b
yun9017 <yun9017ie@naver.com>
2025-10-16 15:58:37 +0900

    07

--- 07/07/main.c
+++ 07/07/main.c
@@ -7,20 +7,23 @@
 
 #include <stdio.h>
 
-void f(void);
+void sub(void);
 
-int i;
 int main(void)
 {
-    for (i=0; i<5; i++)
-    {
-        f();
-    }
+    int i;
+    for (i=0; i<3; i++)
+        sub();
     return 0;
-}
-    void f(void)
+    }
+
+void sub(void)
 {
-    for (i=0; i<10; i++)
-        printf("#");
+    int auto_count = 0;
+    static int static_count = 0;
+    auto_count++;
+    static_count++;
+    printf("auto_count = %d\n",auto_count);
+    printf("static_count = %d\n",static_count);
 }
 
