152edda23e3398bc770f66bc6ea1ff205ea181b7
yun9017 <yun9017ie@naver.com>
2025-10-16 15:37:27 +0900

    07

--- 07/07/main.c
+++ 07/07/main.c
@@ -7,14 +7,17 @@
 
 #include <stdio.h>
 
+int inc(int counter);
+
 int main(void) {
-    int i;
-    
-    for(i=0; i<5; i++)
-    {
-        static int temp = 1; // static 추가 - 소멸되지 않고 끝까지
-        printf("temp = %d\n", temp);
-        temp++;
-    }
+    int i=10;
+    printf("함수 호출전 i = %d\n", i);
+    inc(i); // i = inc(i)의 경우 반환값 활용 -> +1, i=11 출력
+    printf("함수 호출후 i = %d\n", i);
     return 0;
+}
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
