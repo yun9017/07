8f70843d7625778991d33e273867e988b97286a9
yun9017 <yun9017ie@naver.com>
2025-10-16 15:17:23 +0900

    07

---
+++ 07/07/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  07
+//
+//  Created by 한서윤 on 10/16/25.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int flag = 1;
+    int y;
+    
+    while(flag != 0)
+    {
+        //int y;
+        y = 3;
+        flag = 0;
+    }
+    y = 4;
+}
